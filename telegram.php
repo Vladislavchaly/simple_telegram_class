87a4a69974ff402b9eff107f5c1acaca19d485b2
Vladislavchaly <chaly95@gmail.com>
2020-03-23 18:27:23 +0200

    test

--- telegram.php
+++ telegram.php
@@ -1,8 +1,10 @@
 <?php
-
+// class telegram bot
 require_once "telegrambot.php";
+
 require_once "config.php";
-$telegram = new TelegramBot("759558834:AAEK8E9huakI5RmkytQkyf-6HMSF76a0A8M");
+
+$telegram = new TelegramBot($token);
 
 $data = $telegram->getData();
 
