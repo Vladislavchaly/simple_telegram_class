9f8b30e517223f60d153eba96af674bd63dd18e1
Vladislavchaly <chaly95@gmail.com>
2020-03-23 18:22:17 +0200

    remove setToken

--- telegram.php
+++ telegram.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "telegrambot.php";
-
+require_once "config.php";
 $telegram = new TelegramBot("759558834:AAEK8E9huakI5RmkytQkyf-6HMSF76a0A8M");
 
 $data = $telegram->getData();
