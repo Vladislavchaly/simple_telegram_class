f31a30252c96fa2268c83a879e4a761fbe2cebd6
Vladislavchaly <chaly95@gmail.com>
2020-03-23 18:33:10 +0200

    test

--- webhook.php
+++ webhook.php
@@ -1,9 +1,8 @@
 <?php
 
-$webhook = $_POST["webhook"];
-
 require_once "telegrambot.php";
 require_once "config.php";
+
 $telegram = new TelegramBot($token);
 
-$telegram->setWebhook($webhook);
+$telegram->setWebhook($_POST["webhook"]);
