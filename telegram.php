2a72aa0b952a80035d66c26c74bd69c076d95c87
Vladislavchaly <chaly95@gmail.com>
2020-04-28 10:40:12 +0300

    add 1c class

--- telegram.php
+++ telegram.php
@@ -2,7 +2,7 @@
 // class telegram bot
 require_once "telegrambot.php";
 
-$telegram = new TelegramBot("759558834:AAEK8E9huakI5RmkytQkyf-6HMSF76a0A8M");
+$telegram = new TelegramBot("");
 
 $data = $telegram->getData();
 
